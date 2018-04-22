0ab339a3b5f1a5a640f77b892bb0c10309c332a7
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2018-04-22 13:30:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import cairo
+import rsvg
+import json
+import os
+
+def svg_to_png(svg_path, png_path, width, height):
+    svg = rsvg.Handle(svg_path)
+    unscaled_width = svg.props.width
+    unscaled_height = svg.props.height
+
+    svg_surface = cairo.SVGSurface(None, width, height)
+    svg_context = cairo.Context(svg_surface)
+    svg_context.save()
+    svg_context.scale(width / unscaled_width, height / unscaled_height)
+    svg.render_cairo(svg_context)
+    svg_context.restore()
+
+    svg_surface.write_to_png(png_path)
+    svg_surface.finish()
+
+
+svg_path = '/Users/palmerc/Desktop/promon_shield.svg'
+output_path = '/Users/palmerc/Desktop'
+output_json_path = os.path.join(output_path, 'Contents.json')
+json_file = open('Assets.xcassets/AppIcon.appiconset/Contents.json')
+data = json.load(json_file)
+
+for image in data['images']:
+    idiom = image['idiom']
+    size = image['size']
+    scale = image['scale'].strip('x')
+    (width, height) = size.split('x')
+    filename = os.path.basename(os.path.normpath(svg_path)).lower().strip('.svg')
+    if scale == '1':
+        png_filename = '%s-%s-%s.png' % (filename, idiom, width)
+    else:
+        png_filename = '%s-%s-%s@%s.png' % (filename, idiom, width, scale)
+    png_width = float(width) * float(scale)
+    png_height = float(height) * float(scale)
+    png_path = os.path.join(output_path, png_filename)
+    svg_to_png(svg_path, png_path, png_width, png_height)
+    image['filename'] = png_filename
+
+output_json_file = open(output_json_path, 'w+')
+json.dump(data, output_json_file)
